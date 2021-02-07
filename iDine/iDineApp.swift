60dd64d7397d4d7024f2203c2ce30444d7882eef
Thiago Santiago <thiagoalexandres@gmail.com>
2021-02-07 14:49:42 -0300

    Initial commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Thiago Santiago on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
